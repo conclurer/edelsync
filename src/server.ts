d70e07ae3d86f5b950d711bdb882ab014b13bb66
TomasKostadinov <tomas.kostadinov@conclurer.com>
2020-08-25 10:22:47 +0200

    Fix build scripts

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import express from 'express';
 const app = express()
 const PORT : string|number = process.env.PORT || 5000;
 
-app.use("*",(req, res) =>{
-    res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+app.use("/api/v1",(req, res) =>{
+    res.send({
+        "success" : true
+    });
 });
 
-app.listen(PORT,() => console.log(`hosting @${PORT}`));
+app.listen(PORT,() => console.log(`Starting server on http://localhost:${PORT}`));
