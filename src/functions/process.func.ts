38c7019f1003f4e4116032e2023e1e53373e05e7
TomasKostadinov <tomas.kostadinov@conclurer.com>
2020-08-25 15:58:02 +0200

    Create configuration and loaders

---
+++ src/functions/process.func.ts
@@ -0,0 +1,9 @@
+export function processData(req: any, res: any) {
+    try {
+
+    } catch (e) {
+        res.send({
+            success: false
+        });
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
-const app = express()
-const PORT : string|number = process.env.PORT || 5000;
+import {processData} from './functions/process.func';
 
-app.use("/api/v1",(req, res) =>{
+const app = express()
+const PORT: string | number = process.env.PORT || 5000;
+
+
+app.use('/api/v1/process', (req, res) => processData(req, res));
+app.use('/api/v1', (req, res) => {
     res.send({
-        "success" : true
+        'success': true
     });
 });
 
-app.listen(PORT,() => console.log(`Starting server on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Starting server on http://localhost:${PORT}`));
